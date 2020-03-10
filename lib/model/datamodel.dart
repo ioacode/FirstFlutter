a5de67d33d95c569691d750a996ee2b9ef914ca9
balcomm <balcomm@balcomms-MacBook-Pro.local>
2020-03-10 17:10:57 +0700

    Create New GlitView and list View in Feature New

---
+++ lib/model/datamodel.dart
@@ -0,0 +1,20 @@
+class Country {
+  const Country(this.name, this.flag);
+  final String name;
+  final String flag;
+}
+
+class Mainmenu{
+  const Mainmenu(this.name);
+  final String name;
+}
+
+class Populermenu{
+  const Populermenu(this.name);
+  final String name;
+}
+
+class TitleMenu{
+  const TitleMenu(this.nameTitle);
+  final String nameTitle;
+}
